f91c7ebb7c06758104d8fd3127c0f11d73e6eb5a
Damien Garros <dgarros@gmail.com>
2023-11-09 04:29:45 -0500

    Enable pylint in ruff

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
-import os
 from pathlib import Path
+
 from invoke import Context, task
 
 MAIN_DIRECTORY_PATH = Path(__file__).parent
@@ -9,7 +9,26 @@ MAIN_DIRECTORY_PATH = Path(__file__).parent
 def format(context: Context):
     """Run RUFF to format all Python files."""
 
-    exec_cmd = "ruff format ."
+    exec_cmds = ["ruff format .", "ruff check . --fix"]
+    with context.cd(MAIN_DIRECTORY_PATH):
+        for cmd in exec_cmds:
+            context.run(cmd)
+
+
+@task
+def lint_yaml(context: Context):
+    """Run Linter to check all Python files."""
+    print(" - Check code with yamllint")
+    exec_cmd = "yamllint ."
+    with context.cd(MAIN_DIRECTORY_PATH):
+        context.run(exec_cmd)
+
+
+@task
+def lint_mypy(context: Context):
+    """Run Linter to check all Python files."""
+    print(" - Check code with mypy")
+    exec_cmd = "mypy --show-error-codes nornir_infrahub"
     with context.cd(MAIN_DIRECTORY_PATH):
         context.run(exec_cmd)
 
@@ -17,14 +36,15 @@ def format(context: Context):
 @task
 def lint_ruff(context: Context):
     """Run Linter to check all Python files."""
-
+    print(" - Check code with ruff")
     exec_cmd = "ruff check ."
     with context.cd(MAIN_DIRECTORY_PATH):
         context.run(exec_cmd)
 
 
-@task
-def lint(context: Context):
+@task(name="lint")
+def lint_all(context: Context):
     """Run all linters."""
-
-    lint_ruff(context=context)
+    lint_yaml(context)
+    lint_ruff(context)
+    lint_mypy(context)
