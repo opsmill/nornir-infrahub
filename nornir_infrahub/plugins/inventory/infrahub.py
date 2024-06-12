3a0e4094d755a0b018a50930fabe3923f7b69d0e
Marco Martinez <mmmartinez@bluewin.ch>
2024-06-12 09:10:51 +0200

    check if group is a CoreStandardGroup

--- nornir_infrahub/plugins/inventory/infrahub.py
+++ nornir_infrahub/plugins/inventory/infrahub.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Set, Type, Union
 
 import ruamel.yaml
-from infrahub_sdk import Config, InfrahubClientSync, InfrahubNodeSync, NodeNotFoundError, NodeSchema
+from infrahub_sdk import Config, InfrahubClientSync, InfrahubNodeSync, NodeSchema
 from nornir.core.inventory import (
     ConnectionOptions,
     Defaults,
@@ -222,10 +222,8 @@ class InfrahubInventory:
 
             extracted_groups = []
             for related_node in host_node.member_of_groups.peers:
-                try:
+                if related_node.typename == "CoreStandardGroup":
                     extracted_groups.append(related_node.peer.name.value)
-                except NodeNotFoundError:
-                    continue
 
             for group_mapping in self.group_mappings:
                 attrs = group_mapping.split(".")
