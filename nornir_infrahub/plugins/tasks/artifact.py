bb0dc205b03aa983dd35c1e227e44df8a395b53a
Alex Gittings <agitting96@gmail.com>
2025-02-03 01:10:20 +0000

    Use infrahub name

--- nornir_infrahub/plugins/tasks/artifact.py
+++ nornir_infrahub/plugins/tasks/artifact.py
@@ -146,7 +146,7 @@ def get_artifact(task: Task, artifact: str) -> Result:
         httpx.HTTPStatusError: If the API request fails.
 
     Example:
-        Example getting artifacts from infrahub:
+        Example getting artifacts from Infrahub:
 
         ```python
         from nornir import InitNornir
