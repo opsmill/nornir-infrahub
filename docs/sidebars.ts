d2610f55dbc0a2618f00da0e84e31fb5ea303b3e
Alex Gittings <agitting96@gmail.com>
2025-02-03 00:46:07 +0000

    Automatically document plugins

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -3,6 +3,14 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   nornirSidebar: [
     'nornir/readme',
+    {
+      type: 'category',
+      label: 'Reference',
+      items: [
+        'nornir/references/plugins/infrahub_inventory',
+        'nornir/references/plugins/artifact_tasks',
+      ],
+    }
   ]
 };
 
