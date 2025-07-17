c7ef0062b5a284448e024d70bac4502b6b441aff
Mikhail Yohman <mikhail.yohman@gmail.com>
2025-07-17 06:25:26 -0600

    Add branch option from the inventory to the instantiation of InfrahubClientSync

--- nornir_infrahub/plugins/inventory/infrahub.py
+++ nornir_infrahub/plugins/inventory/infrahub.py
@@ -205,7 +205,7 @@ class InfrahubInventory:
         self.defaults_file = Path(defaults_file).expanduser()
         self.group_file = Path(group_file).expanduser()
 
-        self.client = InfrahubClientSync(config=Config(api_token=token), address=self.address)
+        self.client = InfrahubClientSync(config=Config(api_token=token, default_branch=branch), address=self.address)
 
         schema_mappings = schema_mappings or []
         self.schema_mappings = [SchemaMappingNode(**mapping) for mapping in schema_mappings]
